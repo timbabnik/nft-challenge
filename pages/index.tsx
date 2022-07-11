63e562b7921fe4f1fa67155bebadc15a95db9af6
timbabnik <tim.babnik16@gmail.com>
2022-07-11 20:10:12 +0200

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,81 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link';
+import { sanityClient, urlFor } from "../sanity";
+import { Collection } from "../typings"
 
-const Home: NextPage = () => {
+interface Props {
+  collections: Collection[]
+}
+
+const Home = ({ collections }: Props) => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div style={{backgroundColor: "#14001a"}} className="mx-auto flex flex-col min-h-screen py-20 px-10 2xl:px-0  ">
       <Head>
-        <title>Create Next App</title>
+        <title>NFT Drop</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <h1 className="mb-10 text-2xl font-extralight text-gray-300 px-20">
+                    The 
+                    {" "}<span className="font-extrabold underline decoration-purple-500">TIM BABNIK</span>{" "}
+                     NFT Market Place
+      </h1>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
+      <main className=" p-10 mt-20 ">
+        <div className="grid space-x-3 md:grid lg:grid 2xl:grid-cols-2 items-center">
+          <h1 className="text-white text-6xl font-light 2xl:px-10 2xl:text-8xl">The king of <span className="text-purple-400 font-bold">NFT</span><br/>marketplaces</h1>
+          {collections.map(collection => (
+            <Link href={`/nft/${collection.slug.current}`}>
+            <div className="2xl:flex xl:flex md:flex items-center cursor-pointer transition-all duration-200 hover:scale-105">
+              <img className="h-96 w-60 rounded-2xl object-cover bg-gradient-to-br from-purple-100 to-purple-600 p-2 rounded-xl mt-20 sm:ml-10" src="https://i.postimg.cc/fbmwxcsP/Group-2-11.png" alt="" />
+              <img className="h-96 w-60 rounded-2xl object-cover bg-gradient-to-br from-purple-100 to-purple-600 p-2 rounded-xl ml-10 sm:mt-10" src={"https://i.postimg.cc/50Q2Kh8s/Group-3-3.png"} alt="" />
+              <img className="h-96 w-60 rounded-2xl object-cover bg-gradient-to-br from-purple-100 to-purple-600 p-2 rounded-xl ml-10 mb-10 sm:mt-10" src={"https://i.postimg.cc/SK8PZVJW/Group-3-4.png"} alt="" />
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+              
+            </div>
+            </Link>
+          ))}
         </div>
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
     </div>
   )
 }
 
 export default Home
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const query = `*[_type == "collection"]{
+    _id,
+    title,
+    address,
+    description,
+    nftCollectionName,
+    mainImage {
+      asset
+  },
+  previewImage {
+    asset
+  },
+  slug {
+    current
+  },
+  creator-> {
+        _id,
+        name,
+        address,
+        slug {
+          current
+      },
+    },
+  }`
+
+  const collections = await sanityClient.fetch(query)
+  
+  return {
+    props: {
+      collections,
+    }
+  }
+}
---
+++ pages/nft/[id].tsx
@@ -0,0 +1,236 @@
+import React, { useEffect, useState } from 'react'
+import { useAddress, useDisconnect, useMetamask, useNFTDrop } from "@thirdweb-dev/react";
+import { GetServerSideProps } from 'next'
+import { sanityClient, urlFor } from '../../sanity';
+import { Collection } from '../../typings';
+import Link from 'next/link';
+import { BigNumber } from 'ethers';
+import toast, { Toaster } from "react-hot-toast"
+
+interface Props {
+    collection: Collection
+}
+
+function NFTDropPage({ collection }: Props) {
+
+    const [claimedSupply, setClaimedSupply] = useState<number>(0);
+    const [totalSupply, setTotalSupply] = useState<BigNumber>();
+    const [priceInEth, setPriceInEth] = useState<string>();
+    const [loading, setLoading] = useState<boolean>(true);
+    const nftDrop = useNFTDrop(collection.address);
+
+    const connectWithMetamask = useMetamask();
+    const address = useAddress();
+    const disconnect = useDisconnect();
+
+
+
+
+    useEffect(() => {
+        if (!nftDrop) return;
+
+        const fetchPrice =  async() => {
+            const claimConditions = await nftDrop.claimConditions.getAll();
+            setPriceInEth(claimConditions?.[0].currencyMetadata.displayValue)
+        }
+
+        fetchPrice();
+    }, [nftDrop])
+
+    useEffect(() => {
+        if (!nftDrop) return;
+
+        const fetchNFTDropData = async () => {
+            setLoading(true);
+
+            const claimed = await nftDrop.getAllClaimed();
+            const total = await nftDrop.totalSupply();
+
+            setClaimedSupply(claimed.length);
+            setTotalSupply(total);
+
+            setLoading(false);
+        }
+
+        fetchNFTDropData();
+    }, [nftDrop])
+
+
+    const mintNft = () => {
+        if (!nftDrop || !address) return;
+
+        const quantity = 1;
+
+        setLoading(true);
+        const notification = toast.loading("Minting...", {
+            style: {
+                background: "white",
+                color: "green",
+                fontWeight: "bolder",
+                fontSize: "17px",
+                padding: "20px",
+            }
+        })
+
+        nftDrop.claimTo(address, quantity).then(async (tx) => {
+            const receipt = tx[0].receipt
+            const claimedTokenId = tx[0].id
+            const claimedNFT = await tx[0].data()
+
+            toast("HOORAY.. You have Successfully Minted", {
+                duration: 8000,
+                style: {
+                    background: "green",
+                    color: "white",
+                    fontWeight: "bolder",
+                    fontSize: "17px",
+                    padding: "20px",
+                }
+            })
+
+            console.log(receipt);
+            console.log(claimedTokenId);
+            console.log(claimedNFT);
+            
+        })
+        .catch((err) => {
+            console.log(err);
+            toast("Whoops... Something went wrong!", {
+                style: {
+                   background: "red",
+                   color: "white",
+                   fontWeight: "bolder",
+                   fontSize: "17px",
+                   padding: "20px", 
+                }
+            })
+            
+        })
+        .finally(() => {
+            setLoading(false)
+            toast.dismiss(notification)
+        })
+    }
+    
+
+
+  return (
+    <div className="flex h-screen flex-col lg:grid lg:grid-cols-10">
+        <Toaster position="bottom-center"/>
+
+        <div className="bg-gradient-to-br from-purple-900 to-black lg:col-span-4">
+            <div className="flex flex-col items-center justify-center py-2 lg:min-h-screen">
+                <div className="bg-gradient-to-br from-purple-100 to-purple-600 p-2 rounded-xl">
+                    <img className="w-44 rounded-xl object-cover lg:h-96 lg:w-72" src={"https://i.postimg.cc/50Q2Kh8s/Group-3-3.png"} alt="" />
+                </div>
+                <div className="text-center p-5 space-y-2">
+                    <h1 className="text-4xl font-bold text-white">
+                        KING´S CROWN
+                    </h1>
+                    <h2 className="text-xl text-gray-300">
+                        A collection of crown´s that makes you a KING of your own destiny
+                    </h2>
+                </div>
+            </div>
+        </div>
+
+        <div className="flex flex-1 flex-col p-12 lg:col-span-6">
+            <header className="flex items-center justify-between">
+                <Link href={"/"}>
+                    <h1 className="w-25 cursor-pointer text-xl font-extralight sm:w-80">
+                        The 
+                        {" "}<span className="font-extrabold underline decoration-pink-600/50">TIM BABNIK</span>{" "}
+                        NFT Market Place
+                    </h1>
+                </Link>
+                <button onClick={() => address ? disconnect () : connectWithMetamask()} className="rounded-full bg-rose-400 text-white px-4 py-2 text-xs font-bold lg:px-5 lg:py-3 lg:text-base">
+                   {address ? "Sign Out" : "Sign In"}
+                </button>
+
+
+            </header>
+
+            <hr className="my-2 border" />
+            {address && (
+                <p className="text-center text-sm text-rose-400">You´re logged in with wallet {address.substring(0,5)}...{address.substring(address.length - 5)}</p>
+            )}
+
+            <div className="mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:space-y-0 lg:justify-center">
+                
+                <h1 className="text-3xl font-bold lg:text-5xl lg:font-extrabold">The King´s crown | NFT Drop</h1>
+                {
+                    loading ? (
+                        <p className="pt-2 text-xl text-green-500 animate-pulse">Loading Supply Count ...</p>
+                    ) : (
+                        <p className="pt-2 text-xl text-green-500">{claimedSupply} / {totalSupply?.toString()} NFT´s claimed</p>
+                    )
+                }
+
+                {loading && (
+                    <img className="h-80 w-80 object-contain" src="https://cdn.hackernoon.com/images/0*4Gzjgh9Y7Gu8KEtZ.gif" alt="" />
+                )}
+                
+            </div>
+
+            <button onClick={mintNft} disabled={loading || claimedSupply === totalSupply?.toNumber() || !address} className="h-16 bg-red-600 w-full text-white rounded-full mt-10 font-bold disabled:bg-gray-400">
+                {loading ? (
+                    <>Loading</>
+                ) : claimedSupply === totalSupply?.toNumber() ? (
+                    <>SOLD OUT</>
+                ) : !address ? (
+                    <>Sign in to Mind</>
+                ) : (
+                    <span className="font-bold">Mint NFT ({priceInEth} ETH)</span>
+                )}
+
+                
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default NFTDropPage
+
+export const getServerSideProps: GetServerSideProps = async ({params}) => {
+    const query = `*[_type == "collection" && slug.current == $id][0]{
+        _id,
+        title,
+        address,
+        description,
+        nftCollectionName,
+        mainImage {
+          asset
+      },
+      previewImage {
+        asset
+      },
+      slug {
+        current
+      },
+      creator-> {
+            _id,
+            name,
+            address,
+            slug {
+              current
+          },
+        },
+      }`
+
+    const collection = await sanityClient.fetch(query, {
+        id: params?.id
+    })
+
+    if (!collection) {
+        return {
+            notFound: true
+        }
+    }
+
+    return {
+        props: {
+            collection, 
+        }
+    }
+}
